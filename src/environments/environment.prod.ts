c58ca22b57d9fdc6dde101058fd099dac8ef7348
Andrew Steeve Ramírez Guzmán <Aramirez@intergrupo.com>
2019-11-21 13:12:32 -0500

    Initial Commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  url_api: 'https://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyBG_35wDLukmARO2-ZcqfQXMOJwEWlzj8s',
+    authDomain: 'platzi-storage.firebaseapp.com',
+    databaseURL: 'https://platzi-storage.firebaseio.com',
+    projectId: 'platzi-storage',
+    storageBucket: 'platzi-storage.appspot.com',
+    messagingSenderId: '735142716057',
+    appId: '1:735142716057:web:4c3c715c29aae8730b4e08'
+  }
+};
